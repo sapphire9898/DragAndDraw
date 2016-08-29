6e7b16e9a729736800b65b3615618ef21ae75c6d
Sapphire9898 <yueyangzou@gmail.com>
2016-08-29 17:22:56 -0400

    DrawPinkRectangles

---
+++ app/src/main/java/com/example/yueyangzou/draganddraw/Box.java
@@ -0,0 +1,26 @@
+package com.example.yueyangzou.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by yueyangzou on 16/8/29.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
